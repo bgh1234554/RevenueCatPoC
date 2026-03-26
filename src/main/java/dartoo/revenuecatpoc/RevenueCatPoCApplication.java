88c2281682c0d13bf9ecc2df238cdb2937b4dfa6
baekJiHoon <bgh1234554@gmail.com>
2026-03-27 02:55:48 +0900

    20260327 First Commit, Project Environment Setting

---
+++ src/main/java/dartoo/revenuecatpoc/RevenueCatPoCApplication.java
@@ -0,0 +1,13 @@
+package dartoo.revenuecatpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevenueCatPoCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevenueCatPoCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dartoo/revenuecatpoc/RevenueCatPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package dartoo.revenuecatpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevenueCatPoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
